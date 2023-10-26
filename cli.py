7652ff4ea7406f1d4507c1a564f4887b291ff657
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-10-26 15:17:00 +0200

    Add some extra output

--- cli.py
+++ cli.py
@@ -12,30 +12,41 @@ with open('.api_key', 'r') as f:
     openai.api_key = f.read().strip()
 
 messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": ""},
+#        {"role": "system", "content": "You are a helpful assistant."},
 #        {"role": "assistant", "content": "Who's there?"},
 #        {"role": "user", "content": "Orange."},
-#        And model would proceed with "Orange who?"...
 ]
 
 
 def user_input_dict(prompt):
     return {"role": "user", "content": prompt}
 
-
+price = 0
 while True:
-    user_input = input("> ")
+    user_input = input("You: ")
     if not user_input:
-        continue  # accidental empty input maybe...
+        continue  # accidental empty input, next try
     if user_input in ('q', 'x', 'quit', 'exit'):
         break
-    
+
     messages.append(user_input_dict(user_input))
 
     response = openai.ChatCompletion.create(
         model=MODEL, messages=messages, temperature=0.1)
 
     message = response.choices[0]["message"]
-    print('>>', message["content"].strip())
-    
     messages.append(message)
+    print(MODEL + ":", message["content"].strip())
+
+    usage = response.usage
+    prompt_tokens, completion_tokens, total_tokens = (
+        usage.prompt_tokens, usage.completion_tokens, usage.total_tokens)
+
+    # GPT-4 prices in USD, source: https://openai.com/pricing#language-models
+    price += 0.03 / 1e3 * prompt_tokens + 0.06 / 1e3 * completion_tokens
+
+    info = (
+        f'Last prompt / response: {prompt_tokens} / {completion_tokens} tokens, '
+        f'Total price: {price:.3f} USD')
+    print(info)

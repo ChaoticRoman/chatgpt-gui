5f60e24a0865505491cd8afd8d64058afa596da6
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-11-24 13:17:32 +0100

    Update model used to the latest one

--- core.py
+++ core.py
@@ -3,8 +3,14 @@ from dataclasses import dataclass
 
 import openai
 
-MODEL = "gpt-4"
-TEMPERATURE = 0.0
+MODEL = "gpt-4-1106-preview"
+
+# GPT-4 prices in USD, source:
+# https://openai.com/pricing#language-models
+USD_PER_INPUT_TOKEN = 0.01e-3
+USD_PER_OUTPUT_TOKEN = 0.03e-3
+
+TEMPERATURE = 0.1
 
 
 def load_key():
@@ -55,9 +61,9 @@ class GptCore:
             usage = response.usage
             prompt_tokens, completion_tokens = (
                 usage.prompt_tokens, usage.completion_tokens)
-            # GPT-4 prices in USD, source:
-            # https://openai.com/pricing#language-models
-            price += 0.03 / 1e3 * prompt_tokens + 0.06 / 1e3 * completion_tokens  # noqa: E501
+
+            price += USD_PER_INPUT_TOKEN * prompt_tokens
+            price += USD_PER_OUTPUT_TOKEN * completion_tokens
 
             self.output(content, Info(prompt_tokens, completion_tokens, price))
 

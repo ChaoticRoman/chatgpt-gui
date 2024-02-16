147cd6990b878cc445fce11caa7cdd1f2cff9744
Roman Pavelka <roman.pavelka.asi@gmail.com>
2024-02-16 11:18:39 +0100

    Fix formatting in unrelated file

--- dale.py
+++ dale.py
@@ -8,11 +8,11 @@ load_key()
 client = OpenAI()
 
 response = client.images.generate(
-  model="dall-e-3",
-  prompt="Bender plays table foosball against guy in Guy Fawkes in his mask, cartoon styled with score 40 to 35 for Bender",
-  size="1024x1024",
-  quality="standard",
-  n=1,
+    model="dall-e-3",
+    prompt="Bender plays table foosball against guy in Guy Fawkes in his mask, cartoon styled with score 40 to 35 for Bender",
+    size="1024x1024",
+    quality="standard",
+    n=1,
 )
 
 url = response.data[0].url

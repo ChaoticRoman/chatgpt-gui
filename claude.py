13697508bf26129e03a0a5e6288cf5ae25c143ff
Roman Pavelka <roman.pavelka.asi@gmail.com>
2024-03-05 21:45:44 +0100

    Claude sample

---
+++ claude.py
@@ -0,0 +1,25 @@
+import anthropic
+
+with open(".api_key_claude") as f:
+    api_key = f.read().strip()
+
+client = anthropic.Anthropic(
+    api_key=api_key,
+)
+message = client.messages.create(
+    model="claude-3-opus-20240229",
+    max_tokens=1000,
+    temperature=0,
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Tell a joke."
+                }
+            ]
+        }
+    ]
+)
+print(message.content)

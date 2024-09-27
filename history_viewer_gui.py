7deb1e08e994541576d350545f466098936005a8
Roman Pavelka <roman.pavelka.asi@gmail.com>
2024-09-27 17:12:27 +0200

    Some work done on history viewer

--- history_viewer_gui.py
+++ history_viewer_gui.py
@@ -2,6 +2,13 @@ import os
 import json
 import tkinter as tk
 from tkinter import Listbox, Scrollbar, Text, END, RIGHT, Y, LEFT, BOTH
+import html
+
+from tkinterweb import HtmlFrame
+from misaka import Markdown, HtmlRenderer
+from pygments import highlight
+from pygments.formatters import HtmlFormatter, ClassNotFound
+from pygments.lexers import get_lexer_by_name
 
 from core import DATA_DIRECTORY
 
@@ -13,28 +20,20 @@ class JsonViewerApp(tk.Tk):
         self.title("JSON Viewer")
         self.geometry("800x600")
 
-        # Create the left frame for the list of files
         self.left_frame = tk.Frame(self)
         self.left_frame.pack(side=LEFT, fill=tk.Y)
 
-        # Create the right frame for the file content
         self.right_frame = tk.Frame(self)
         self.right_frame.pack(side=LEFT, fill=BOTH, expand=True)
 
-        # Listbox to display the list of JSON files
         self.file_listbox = Listbox(self.left_frame, width=40)
         self.file_listbox.pack(side=LEFT, fill=tk.Y)
-
-        # Scrollbar for the listbox
         self.scrollbar = Scrollbar(self.left_frame)
         self.scrollbar.pack(side=RIGHT, fill=Y)
-
-        # Attach the scrollbar to the listbox
         self.file_listbox.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.file_listbox.yview)
 
-        # Text widget to display the content of the selected file
-        self.file_content_text = Text(self.right_frame, wrap=tk.WORD)
+        self.file_content_text = HtmlFrame(self.right_frame, messages_enabled = False)
         self.file_content_text.pack(fill=BOTH, expand=True)
 
         # Load the list of JSON files
@@ -43,10 +42,14 @@ class JsonViewerApp(tk.Tk):
         # Bind the listbox selection event to a function
         self.file_listbox.bind("<<ListboxSelect>>", self.on_file_select)
 
+        self.md = Markdown(HighlighterRenderer(), extensions=('fenced-code',))
+
     def load_json_files(self):
         """Load the list of JSON files from the hardcoded folder."""
         if os.path.exists(DATA_DIRECTORY):
-            json_files = [f for f in os.listdir(DATA_DIRECTORY) if f.endswith(".json")]
+            json_files = sorted(
+                [f for f in os.listdir(DATA_DIRECTORY) if f.endswith(".json")])
+            print(json_files[-1])
             for file in json_files:
                 self.file_listbox.insert(END, file)
         else:
@@ -62,18 +65,32 @@ class JsonViewerApp(tk.Tk):
             # Construct the full file path
             file_path = os.path.join(DATA_DIRECTORY, selected_file)
 
-            # Read and display the content of the selected file
-            try:
-                with open(file_path, "r") as file:
-                    file_content = json.load(file)
-                    formatted_content = json.dumps(file_content, indent=4)
+            with open(file_path, "r") as file:
+                file_content = json.load(file)
+                formatted_content = self.md(format_json(file_content))
+                # print(formatted_content)
+                self.file_content_text.load_html(formatted_content)
 
-                    # Clear the text widget and insert the new content
-                    self.file_content_text.delete(1.0, END)
-                    self.file_content_text.insert(END, formatted_content)
-            except Exception as e:
-                self.file_content_text.delete(1.0, END)
-                self.file_content_text.insert(END, f"Error reading file: {e}")
+
+class HighlighterRenderer(HtmlRenderer):
+    def blockcode(self, text, lang):
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+        except ClassNotFound:
+            lexer = None
+
+        if lexer:
+            formatter = HtmlFormatter(full=True, wrapcode=True, nobackground=False,
+                                      style="xcode", prestyles="font-family: monospace")
+            return highlight(text, lexer, formatter)
+        # default
+        return '\n<pre style="font-family: monospace">{}</pre>\n'.format(
+                            html.escape(text.strip()))
+
+
+def format_json(j):
+    return "\n\n".join(
+        [f"**{m["role"].upper()}:**\n\n{m["content"]}" for m in j])
 
 
 if __name__ == "__main__":

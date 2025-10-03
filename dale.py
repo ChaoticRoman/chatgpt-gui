e3dd0524bc6c363d3cf65e0cd3287acc18a23ae4
Roman Pavelka <roman.pavelka.asi@gmail.com>
2025-10-03 16:30:45 +0200

    Implement batch processing in dale client

--- dale.py
+++ dale.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import time
+import multiprocessing
+import random, string
 
 from openai import OpenAI
 
@@ -9,19 +11,24 @@ from core import load_key
 load_key()
 client = OpenAI()
 
+def generate(prompt):
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    url = response.data[0].url
+    timestamp = time.strftime("%Y-%m-%dT%H:%M:%S")
+    rnd = ''.join(random.choices(string.ascii_lowercase, k=6))
+    filename = f"output-{timestamp}-{rnd}.png"
+
+    os.system(f'wget -O "{filename}" "{url}"')
+
 prompt = input("Prompt: ")
-print(f'Your prompt is "{prompt}".')
-
-response = client.images.generate(
-    model="dall-e-3",
-    prompt=prompt,
-    size="1024x1024",
-    quality="standard",
-    n=1,
-)
-
-url = response.data[0].url
-timestamp = time.strftime("%Y-%m-%dT%H:%M:%S")
-filename = f"output-{timestamp}.png"
-
-os.system(f'wget -O "{filename}" "{url}"')
+amount = int(input("Amount: "))
+#p = multiprocessing.Pool(10)
+for _ in range(amount):
+    generate(prompt)
